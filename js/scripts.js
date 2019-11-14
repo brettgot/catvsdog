af98c93f2145e27de27714f0bb20cf0a147c366d
Student <example@example.com>
2019-11-13 20:48:06 -0800

    added pics and fightin' words for dog and cat

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("h1").click(function(){
+  $("ul#catfight").append("<li>Meow</li>");
+});
+$("h1").click(function(){
+$("ul#dogfight").append("<li>Bark</li>");
+});
+
+
+
+});
